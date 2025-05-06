5af6a6f5bafda90db115fd49a104302b1613561d
PanagiotisChatzoglou <pegadeli12345@gmail.com>
2025-05-06 23:43:48 +0300

    Creating mondels (category and course) and queries

--- app/page.js
+++ app/page.js
@@ -1,12 +1,27 @@
 import Image from "next/image";
 import "./globals.css";
 import Text from "@/components/Text";
+import { getCourses } from "@/queries/courses";
 
-export default function Home() {
+export default async function Home() {
+  const courses = await getCourses();
+  console.log(courses);
   return (
-    <h1>
-      Hello, Next.js!
-      <Text />
-    </h1>
+    <>
+      <h1>
+        Hello, Next.js!
+        <Text />
+      </h1>
+      <div className="grid grid-cols-3 gap-4">
+        {courses.map((course) => (
+          <div key={course._id} className="border p-4">
+            <h2>{course.title}</h2>
+            <p>{course.subtitle}</p>
+            <p>{course.description}</p>
+            <p>Category: {course?.category?.title}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
---
+++ model/category-model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const categorySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Category =
+  mongoose.models.Category ?? mongoose.model("Category", categorySchema);
