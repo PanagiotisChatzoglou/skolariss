7c12a0480b740b55f6f1ffcf0e645f201868126e
PanagiotisChatzoglou <pegadeli12345@gmail.com>
2025-06-16 23:12:24 +0300

    enroll button component

--- app/(main)/courses/_components/CourseCard.jsx
+++ app/(main)/courses/_components/CourseCard.jsx
@@ -1,3 +1,4 @@
+import EnrollCourse from "@/components/enroll-course";
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/formatPrice";
 import { ArrowRight, BookOpen } from "lucide-react";
@@ -44,10 +45,7 @@ const CourseCard = ({ course }) => {
               {formatPrice(course?.price)}
             </p>
 
-            <Button variant="ghost" className="text-xs text-sky-700 h-7 gap-1">
-              Enroll
-              <ArrowRight className="w-3" />
-            </Button>
+            <EnrollCourse asLink={true} />
           </div>
         </div>
       </div>
