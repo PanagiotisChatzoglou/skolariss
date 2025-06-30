d646acc5f0f7d60d1cbc16d051e26e490931e515
PanagiotisChatzoglou <pegadeli12345@gmail.com>
2025-07-01 00:00:26 +0300

    added changePassword action

--- app/actions/account.js
+++ app/actions/account.js
@@ -1,6 +1,8 @@
 "use server";
 import { User } from "@/model/user-model";
+import { validatePassword } from "@/queries/users";
 import { revalidatePath } from "next/cache";
+import bcrypt from "bcryptjs";
 
 export async function updateUserInfo(email, updatedData) {
   try {
@@ -12,4 +14,23 @@ export async function updateUserInfo(email, updatedData) {
   }
 }
 
-export async function changePassword(email, oldPassword, newPassword) {}
+export async function changePassword(email, oldPassword, newPassword) {
+  const isMatch = await validatePassword(email, oldPassword);
+
+  if (!isMatch) {
+    throw new Error("Please enter a valid current password");
+  }
+  const filter = { email: email };
+  const hashedPassword = await bcrypt.hash(newPassword, 5);
+
+  const dataToUpadate = {
+    password: hashedPassword,
+  };
+
+  try {
+    await User.findOneAndUpdate(filter, dataToUpadate);
+    revalidatePath("/account");
+  } catch (error) {
+    throw new Error(error);
+  }
+}
